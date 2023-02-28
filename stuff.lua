da10142c6421bb902890a39c7b48fa363a23cf64
Lee Fall <84427490+LeeFall1@users.noreply.github.com>
2023-02-28 21:33:15 +0000

    Create stuff.lua

---
+++ stuff.lua
@@ -0,0 +1,171 @@
+--LEEFALL EZ Light Programmer
+
+light1TimeShift = 0.111111111 --export: light1 time shift
+light2TimeShift = 0.222222222 --export: light2 time shift
+light3TimeShift = 0.333333333 --export: light3 time shift
+light4TimeShift = 0.444444444 --export: light4 time shift
+light5TimeShift = 0.555555555 --export: light5 time shift
+light6TimeShift = 0.666666666 --export: light6 time shift
+light7TimeShift = 0.777777777 --export: light7 time shift
+light8TimeShift = 0.888888888 --export: light8 time shift
+light9TimeShift = 0.999999999 --export: light9 time shift
+light10TimeShift = 1.111111111 --export: light10 time shift
+
+
+
+if slot1 then slot1.setBlinkingTimeShift(light1TimeShift) end 
+if slot2 then slot2.setBlinkingTimeShift(light2TimeShift) end  
+if slot3 then slot3.setBlinkingTimeShift(light3TimeShift) end  
+if slot4 then slot4.setBlinkingTimeShift(light4TimeShift) end  
+if slot5 then slot5.setBlinkingTimeShift(light5TimeShift) end  
+if slot6 then slot6.setBlinkingTimeShift(light6TimeShift) end  
+if slot7 then slot7.setBlinkingTimeShift(light7TimeShift) end 
+if slot8 then slot8.setBlinkingTimeShift(light8TimeShift) end  
+if slot9 then slot9.setBlinkingTimeShift(light9TimeShift) end 
+if slot10 then slot10.setBlinkingTimeShift(light10TimeShift) end  
+
+
+
+light1BlinkState = true --export: light1 blink state
+light2BlinkState = true --export: light2 blink state
+light3BlinkState = true --export: light3 blink state
+light4BlinkState = true --export: light4 blink state
+light5BlinkState = true --export: light5 blink state
+light6BlinkState = true --export: light6 blink state
+light7BlinkState = true --export: light7 blink state
+light8BlinkState = true --export: light8 blink state
+light9BlinkState = true --export: light9 blink state
+light10BlinkState = true --export: light10 blink state
+
+
+if slot1 then slot1.setBlinkingState(light1BlinkState) end
+if slot2 then slot2.setBlinkingState(light2BlinkState) end
+if slot3 then slot3.setBlinkingState(light3BlinkState) end
+if slot4 then slot4.setBlinkingState(light4BlinkState) end
+if slot5 then slot5.setBlinkingState(light5BlinkState) end
+if slot6 then slot6.setBlinkingState(light6BlinkState) end
+if slot7 then slot7.setBlinkingState(light7BlinkState) end
+if slot8 then slot8.setBlinkingState(light8BlinkState) end
+if slot9 then slot9.setBlinkingState(light9BlinkState) end
+if slot10 then slot10.setBlinkingState(light10BlinkState) end
+
+
+light1OnDuration = 1.0 --export: Light 1 On Duration
+light2OnDuration = 1.0 --export: Light 2 On Duration
+light3OnDuration = 1.0 --export: Light 3 On Duration
+light4OnDuration = 1.0 --export: Light 4 On Duration
+light5OnDuration = 1.0 --export: Light 5 On Duration
+light6OnDuration = 1.0 --export: Light 6 On Duration
+light7OnDuration = 1.0 --export: Light 7 On Duration
+light8OnDuration = 1.0 --export: Light 8 On Duration
+light9OnDuration = 1.0 --export: Light 9 On Duration
+light10OnDuration = 1.0 --export: Light 10 On Duration
+
+
+if slot1 then slot1.setOnBlinkingDuration(light1OnDuration) end
+if slot2 then slot2.setOnBlinkingDuration(light2OnDuration) end
+if slot3 then slot3.setOnBlinkingDuration(light3OnDuration) end
+if slot4 then slot4.setOnBlinkingDuration(light4OnDuration) end
+if slot5 then slot5.setOnBlinkingDuration(light5OnDuration) end
+if slot6 then slot6.setOnBlinkingDuration(light6OnDuration) end
+if slot7 then slot7.setOnBlinkingDuration(light7OnDuration) end
+if slot8 then slot8.setOnBlinkingDuration(light8OnDuration) end
+if slot9 then slot9.setOnBlinkingDuration(light9OnDuration) end
+if slot10 then slot10.setOnBlinkingDuration(light10OnDuration) end
+
+light1OffDuration = 1.0 --export: Light 1 Off Duration
+light2OffDuration = 1.0 --export: Light 2 Off Duration
+light3OffDuration = 1.0 --export: Light 3 Off Duration
+light4OffDuration = 1.0 --export: Light 4 Off Duration
+light5OffDuration = 1.0 --export: Light 5 Off Duration
+light6OffDuration = 1.0 --export: Light 6 Off Duration
+light7OffDuration = 1.0 --export: Light 7 Off Duration
+light8OffDuration = 1.0 --export: Light 8 Off Duration
+light9OffDuration = 1.0 --export: Light 9 Off Duration
+light109OffDuration = 1.0 --export: Light 10 Off Duration
+
+if slot1 then slot1.setOffBlinkingDuration(light1OffDuration) end
+if slot2 then slot2.setOffBlinkingDuration(light2OffDuration) end
+if slot3 then slot3.setOffBlinkingDuration(light3OffDuration) end
+if slot4 then slot4.setOffBlinkingDuration(light4OffDuration) end
+if slot5 then slot5.setOffBlinkingDuration(light5OffDuration) end
+if slot6 then slot6.setOffBlinkingDuration(light6OffDuration) end
+if slot7 then slot7.setOffBlinkingDuration(light7OffDuration) end
+if slot8 then slot8.setOffBlinkingDuration(light8OffDuration) end
+if slot9 then slot9.setOffBlinkingDuration(light9OffDuration) end
+if slot10 then slot10.setOffBlinkingDuration(light10OffDuration) end
+
+
+light1R = 1 --export: (Default: 1)
+light1G = 1 --export: (Default: 1)
+light1B = 1 --export: (Default: 1)
+
+light2R = 1 --export: (Default: 1)
+light2G = 1 --export: (Default: 1)
+light2B = 1 --export: (Default: 1)
+
+light3R = 1 --export: (Default: 1)
+light3G = 1 --export: (Default: 1)
+light3B = 1 --export: (Default: 1)
+
+light4R = 1 --export: (Default: 1)
+light4G = 1 --export: (Default: 1)
+light4B = 1 --export: (Default: 1)
+
+light5R = 1 --export: (Default: 1)
+light5G = 1 --export: (Default: 1)
+light5B = 1 --export: (Default: 1)
+
+light6R = 1 --export: (Default: 1)
+light6G = 1 --export: (Default: 1)
+light6B = 1 --export: (Default: 1)
+
+light7R = 1 --export: (Default: 1)
+light7G = 1 --export: (Default: 1)
+light7B = 1 --export: (Default: 1)
+
+light8R = 1 --export: (Default: 1)
+light8G = 1 --export: (Default: 1)
+light8B = 1 --export: (Default: 1)
+
+light9R = 1 --export: (Default: 1)
+light9G = 1 --export: (Default: 1)
+light9B = 1 --export: (Default: 1)
+
+light10R = 1 --export: (Default: 1)
+light10G = 1 --export: (Default: 1)
+light10B = 1 --export: (Default: 1)
+
+
+if slot1 then
+slot1.setColor(light1R,light1G,light1B)
+    end
+if slot2 then
+slot2.setColor(light2R,light2G,light2B)
+    end
+if slot3 then
+slot3.setColor(light3R,light3G,light3B)
+    end    
+if slot4 then    
+slot4.setColor(light4R,light4G,light4B)
+    end    
+if slot5 then        
+slot5.setColor(light5R,light5G,light5B)
+    end    
+if slot6 then            
+slot6.setColor(light6R,light6G,light6B)
+    end    
+if slot7 then                
+slot7.setColor(light7R,light7G,light7B)
+    end    
+if slot8 then                    
+slot8.setColor(light8R,light8G,light8B)
+    end    
+if slot9 then                        
+slot9.setColor(light9R,light9G,light9B)
+    end   
+if slot10 then                        
+slot10.setColor(light10R,light10G,light10B)
+end  
+
+
